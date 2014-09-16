07dab8e098c11f958894417565a25b02082598e0
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-09-15 21:12:32 -0400

    Add fixed_len_write

--- library.cc
+++ library.cc
@@ -1,6 +1,11 @@
 #include "library.h"
 
-
 int fixed_len_sizeof(Record *record){
     return record_size;
+}
+
+void fixed_len_write(Record *record, void *buf) {
+	for (int i = 0; i < record->size(); i++) {
+		memcopy(buf[i*attribute_len], record->at(i), attribute_len);
+	}
 }
