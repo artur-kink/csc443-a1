fe21f3b5fc1f783b0a9d37c538d42641f5dc73b4
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-09-15 21:41:42 -0400

    Fix fixed_len_write iteration, start work on fixed_len_read

--- library.cc
+++ library.cc
@@ -5,7 +5,14 @@ int fixed_len_sizeof(Record *record){
 }
 
 void fixed_len_write(Record *record, void *buf) {
-	for (int i = 0; i < record->size(); i++) {
-		memcopy(buf[i*attribute_len], record->at(i), attribute_len);
+	for (int i = 0; i < num_attributes; i++) {
+		memcpy((V)buf + i*attribute_len, record->at(i), attribute_len);
+	}
+}
+
+void fixed_len_read(void *buf, int size, Record *record) {
+	for (int i = 0; i < num_attributes; i++) {
+		// V* thing = new V[attribute_len];
+		// memcpy(thing, buf + i*attribute_len, attribute_len);
 	}
 }
