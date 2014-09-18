e35825d1f2aa431538d7fb55519939155860f3ef
Joseph Kahn <JosephBKahn@gmail.com>
2014-09-17 23:25:11 -0400

    put back read back print

--- read_fixed_len_page.cc
+++ read_fixed_len_page.cc
@@ -12,39 +12,39 @@ int main(int argc, char** argv){
         printf("Usage: read_fixed_len_page <page_file> <page_size>\n");
         return 1;
     }
-    
+
     //Parse arguments.
     FILE* page_file = fopen(argv[1], "rb");
     int page_size = atoi(argv[2]);
-    
+
     //Record program start time.
     struct timeb t;
     ftime(&t);
     long start_ms = t.time * 1000 + t.millitm;
-    
+
     Page* page = new Page;
     //Read all pages in file.
     while(!feof(page_file)){
-        
+
         //Initialize a page to read into.
         init_fixed_len_page(page, page_size, record_size);
-        
+
         //If a page could not be read were done.
         if(fread(page->data, page_size, 1, page_file) == 0){
             break;
         }
         else{
-            
+            printf("Read page.\n");
             unsigned char* directory_offset = ((unsigned char*)page->data) + fixed_len_page_directory_offset(page);
-            
+
             int record_count = 0;
-            
+
             //Traverse all records in read page and print them.
             for(int i = 0; i < fixed_len_page_capacity(page); i++){
                 if(i > 0 && i%8 == 0){
                     directory_offset += 1;
                 }
-                
+
                 //Retrieve and print record from slot if its marked in the directory.
                 unsigned char directory = (unsigned char)*directory_offset;
                 if(directory >> (i%8) & 0x01){
@@ -54,13 +54,13 @@ int main(int argc, char** argv){
                     print_record(&record);
                     record_count++;
                 }
-            
+
             }
         }
         free_fixed_len_page(page);
     }
     free(page);
-    
+
     //Calculate program end time.
     ftime(&t);
     long end_ms = t.time * 1000 + t.millitm;
