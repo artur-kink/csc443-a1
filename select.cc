219e8ebaf01fad16f0a89f7069f4e48372d4f846
Artur Kink <artur.kink@gmail.com>
2014-09-26 13:36:24 -0400

    Proper commenting for select.

--- select.cc
+++ select.cc
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <sys/timeb.h>
 
+/**
+ * Select a substring(first five letters) of a given attribute, <attribute_id> such
+ * that the attribute >= <start> and attribute <= <end>.
+ */
 int main(int argc, char** argv) {
     //Check for all arguments.
     if (argc != 6) {
@@ -16,37 +20,41 @@ int main(int argc, char** argv) {
     char* start = argv[3];
     char* end = argv[4];
     
+    //Open heap file.
     FILE* heap_file = fopen(argv[1], "rb");
     if (!heap_file) {
         printf("Failed to open heap file: %s\n", argv[1]);
         return 2;
     }
 
-    // Read in start and end
-    Heapfile* heap = (Heapfile*) malloc(sizeof (Heapfile));
-    init_heapfile(heap, atoi(argv[5]), heap_file);
-
-    RecordIterator* recordi = new RecordIterator(heap);
-
     //Record Start Time 
     struct timeb t;
     ftime(&t);
     long start_ms = t.time * 1000 + t.millitm;
 
+    //Initialize heap and record iterator from file.
+    Heapfile* heap = (Heapfile*) malloc(sizeof (Heapfile));
+    init_heapfile(heap, atoi(argv[5]), heap_file);
+    RecordIterator* recordi = new RecordIterator(heap);
+    
     //Find all records matching query.
     int number_of_records_matching_query = 0;
     int total_number_of_records = 0;
     while (recordi->hasNext()) {
         Record next_record = recordi->next();
+        //Check if attribute in selection range.
         if(strcmp(next_record.at(attribute_id), start) >= 0 && strcmp(next_record.at(attribute_id), end) <= 0){
             printf("%.5s\n", next_record.at(attribute_id));
-            total_number_of_records++;
+            number_of_records_matching_query++;
         }
+        total_number_of_records++;
     }
     
+    //Calculate program runtime.
     ftime(&t);
     long end_ms = t.time * 1000 + t.millitm;
 
+    //Print metrics.
     printf("TIME: %lu\n", end_ms - start_ms);
     printf("TOTAL NUMBER OF RECORDS : %d\n", total_number_of_records);
     printf("TOTAL NUMBER OF RECORDS SELECTED: %d\n", number_of_records_matching_query);
