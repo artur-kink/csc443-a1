5cd42d940742b6f498f8edd28ae9846d0c77a89a
Joseph Kahn <JosephBKahn@gmail.com>
2014-10-03 15:36:47 -0400

    removing print statements

--- library.cc
+++ library.cc
@@ -366,7 +366,6 @@ bool RecordIterator::hasNext() {
         while (this->current_slot < fixed_len_page_capacity(this->current_page)) {
             int dir_slot_offset = *(int*) (((char*)this->current_page->data) + fixed_len_page_directory_offset(this->current_page) + (char)floor(this->current_slot / 8));
             int directory_bit_for_slot = ((dir_slot_offset >> (current_slot % 8)) & 1);
-            printf("directory bit at slot %d for pid %d has a %d\n", current_slot, this->current_page_id, directory_bit_for_slot);
             if ((directory_bit_for_slot != 0)) {
                 break;
             }
@@ -374,7 +373,6 @@ bool RecordIterator::hasNext() {
         }
         // If we are above the slot capacity, we read in the next page.
         if (this->current_slot == fixed_len_page_capacity(this->current_page)) {
-            printf("next page\n");
             this->current_slot = 0;
             this->current_page_id++;
 
