4bae0a6db424c020aa11d6e41db1a625426ea691
Alexander Biggs <alexanderkbiggs@gmail.com>
2014-10-17 09:39:19 -0400

    take filename, usage

--- generategiant.sh
+++ generategiant.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-python mkcsv.py genlots.csv 5000
-cat genlots.csv > lots.csv
-while [ $(stat -f %z lots.csv) -lt 1050000000 ]
+if [ $# -lt 2 ]
+    then
+        echo "Usage: $0 <filename>"
+        exit 1
+fi
+
+python mkcsv.py _temp.csv 5000
+cat genlots.csv > $1
+while [ $(stat -f %z $1) -lt 1050000000 ]
 do
-    cat genlots.csv >> lots.csv
+    cat _temp.csv >> $1
 done
+
+rm _temp.csv
